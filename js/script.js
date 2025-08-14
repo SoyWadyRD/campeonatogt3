134c5cca7840b9924d1ba9c0c49930ffdf998eb3
SoyWadyRD <andreslopez261316@gmail.com>
2025-08-14 10:59:14 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Contador
+const countdownDate = new Date("August 16, 2025 20:00:00 GMT-0400").getTime();
+
+const x = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  const days = Math.floor(distance / (1000*60*60*24));
+  const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+  const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+  const seconds = Math.floor((distance % (1000*60)) / 1000);
+
+  document.getElementById("dias").innerText = days;
+  document.getElementById("horas").innerText = hours;
+  document.getElementById("minutos").innerText = minutes;
+  document.getElementById("segundos").innerText = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("contador").innerText = "¡La carrera ha comenzado!";
+  }
+}, 1000);
+
+// Lightbox autos
+const zoomImgs = document.querySelectorAll(".zoom");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.querySelector(".lightbox-img");
+const cerrar = document.querySelector(".cerrar");
+
+zoomImgs.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+cerrar.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+  if(e.target !== lightboxImg) lightbox.style.display = "none";
+});
